c8f345d247d97be50755658afc4b28594d29e19b
luh-alves <luh.ana.af@gmail.com>
2021-09-28 12:04:47 -0300

    Trilha/Beta - atividades e desafios do segundo curso .Net/finalizado

---
+++ ByteBank7/ContaCorrente.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank7
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+       public static int TotalDeContasCriadas { get; private set; }
+
+        private int _agencia;
+        public int Agencia 
+        {
+            get 
+            {
+                return _agencia;
+            }
+            set 
+            { 
+                if(value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+
+
+     
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo = _saldo - valor;
+                return true;
+            }
+        }
+
+
+
+        //quando tem void , não precisa de retorno
+        public void Depositar(double valor)
+        {
+            _saldo = _saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo = _saldo - valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
+
---
+++ ByteBank7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta = new ContaCorrente(867, 123456);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaGabi = new ContaCorrente(867, 123456);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
